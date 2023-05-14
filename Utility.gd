96bfe1f8b6362160e61f2c43e6e2c5259a06dcc5
Rakka Rage <rakkarage@gmail.com>
2023-05-14 00:34:16 -0400

    x

--- Utility.gd
+++ Utility.gd
@@ -14,6 +14,17 @@ static func position(i: int, width: int) -> Vector2:
 	var x := int(i - width * y)
 	return Vector2(x, y)
 
+static func constrainRect(world: Rect2, map: Rect2) -> Vector2:
+	return constrain(world.position, world.end, map.position, map.end)
+
+static func constrain(minWorld: Vector2, maxWorld: Vector2, minMap: Vector2, maxMap: Vector2) -> Vector2:
+	var delta := Vector2.ZERO
+	if minWorld.x > minMap.x: delta.x += minMap.x - minWorld.x
+	if maxWorld.x < maxMap.x: delta.x -= maxWorld.x - maxMap.x
+	if minWorld.y > minMap.y: delta.y += minMap.y - minWorld.y
+	if maxWorld.y < maxMap.y: delta.y -= maxWorld.y - maxMap.y
+	return delta
+
 static func arraySequence(value: int, count: int) -> Array:
 	var array := []
 	for i in range(count):
