1bb126d37acef266ffb5d34efd573f4566c00922
Rakka Rage <rakkarage@gmail.com>
2023-06-20 13:52:24 -0400

    x

--- AutoFileDialog.gd
+++ AutoFileDialog.gd
@@ -10,12 +10,12 @@ func showSave(ok: Callable, filter: Array[String]) -> void:
 	return showDialog(FileDialog.FILE_MODE_SAVE_FILE, ok, filter)
 
 ## Show a dialog with custom values.
-func showDialog(fileMode: FileDialog.FileMode, ok: Callable, filter: Array[String] = ["*.* ; All Files"], file := str(Random.next(1000000)), path := OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)) -> void:
+func showDialog(fileMode: FileDialog.FileMode, ok: Callable, filter: Array[String] = ["*.* ; All Files"], file := "file" + str(Random.next(1000000)), path := OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)) -> void:
 	access = FileDialog.ACCESS_FILESYSTEM
 	file_mode = fileMode
 	current_dir = path
+	current_file = file
 	filters = filter
-	file = file
 	size = Vector2(800, 600)
 	popup_centered()
 	connect("file_selected", ok)
