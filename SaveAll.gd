34c25072bb5c58f6f1a048cfb933ab42ee8b57a2
Rakka Rage <rakkarage@gmail.com>
2023-06-28 13:37:14 -0400

    x

--- SaveAll.gd
+++ SaveAll.gd
@@ -1,10 +1,8 @@
-## This script is an editor tool that saves all scenes and resources in the project.
-## Right click on script in the Godot script editor and select "Run" to run the script.
+## Resaves all scenes and resources in the project.
+## Right click on script in the Godot script editor and select "Run" to execute the script.
 @tool
 extends EditorScript
 
-## Iterate over all `.tscn` and `.tres` files in the project directory. See [method Utility.listFiles].
-## For each file, loads [method @GDScript.load] and saves using [method ResourceSaver.save].
 func _run() -> void:
 	for file in Utility.list_files("res://", ["tscn", "tres"]):
 		ResourceSaver.save(load(file))
